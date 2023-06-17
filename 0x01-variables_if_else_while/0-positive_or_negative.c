732e1d4e491bf0868821b5feb8a163bd6b5e27e2
Paul Danso <pauldasare@gmail.com>
2023-06-16 22:03:27 -0700

    time strand

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  *main - assign random number to variables
  *Description: Check whether positive or negative
  * Return: always 0
  */
-
 int main(void)
 {
 	int n;
