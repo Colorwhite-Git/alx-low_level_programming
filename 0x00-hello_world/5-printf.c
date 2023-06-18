298dbacd924294f3548a9fceee1af1968a0a830a
Paul Danso <pauldasare@gmail.com>
2023-06-18 07:07:34 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print exactly
+ * Description: The printf() function is used
+ * Return: Always (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
