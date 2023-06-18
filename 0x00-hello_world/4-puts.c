8d7314e289633e8424bd56bb118bfef3a723f315
Paul Danso <pauldasare@gmail.com>
2023-06-18 06:15:24 -0700

    Print exactly using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - This prints exact input
+ * Description: The function used is puts()
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
