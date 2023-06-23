a9915852a2ca0df0d1d38fa84f540af31cf11135
Paul Danso <pauldasare@gmail.com>
2023-06-23 13:57:03 -0700

    Straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in the terminal
+ * Description: Use only _putchar function to print
+ * @n: number of times the character '_' should be printed
+ * End line with '\n'
+ */
+
+void print_line(int n)
+{
