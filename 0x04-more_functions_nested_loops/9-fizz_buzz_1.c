2477a047271e62eca010458ee236383c887be9e3
Paul Danso <pauldasare@gmail.com>
2023-06-24 03:52:52 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz_1.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz_1.c
@@ -9,39 +9,29 @@
 
 int main(void)
 {
-	int x;
+	int i;
 
-	x = 1;
-
-	while (x <= 100)
+	for (x = 1; x <= 100; x++)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 
 		else if (x % 3 == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 
 		else if (x % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 
 		else
 		{
 			printf("%d", x);
 		}
-
-		if (x != 100)
-		{
-			printf(" ");
-		}
-
-		x++;
-
 	}
 
 	printf("\n");
