70ba62783dc1b519a219502c6b14f4712037403f
Paul Danso <pauldasare@gmail.com>
2023-06-26 22:22:53 -0700

    All strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * Prototype - for all head files
- */
+#include <stdio.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,8 +9,16 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+int _putchar(char c);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _putchar(char c);
+int _atoi(char *s);
+int fputc(int c, FILE *stream);
+int fputs(const char *s, FILE *stream);
+int putc(int c, FILE *stream);
+int putchar(int c);
+int puts(const char *s);
+
+#endif
