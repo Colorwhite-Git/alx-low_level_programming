2e8bf02866ac54450204b22a1e8b908c781acb69
Paul Danso <pauldasare@gmail.com>
2023-07-06 11:56:31 -0700

    Prints string recursion

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - output a single character to standard output
  * @c: The character to print
  *
  * Return: On success 1.
@@ -10,5 +11,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, !));
+	return write(1, &c, 1);
 }
