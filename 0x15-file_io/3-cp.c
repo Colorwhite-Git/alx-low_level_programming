0bd4c534ec2d6e21192279dd51fe70a68aa5c680
Paul Asare <pauldasare@gmail.com>
2023-08-16 10:10:07 -0700

    Fix correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,92 +1,73 @@
-#define ERR_INVALID_ELF  98
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <elf.h>
 
+void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
- * display_error - Entry point of function
- * Description: It is display error message and exit
- * @msg: Variable used to store string messages
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: arguments passed
  *
- * Return: Always 0.
+ * Return: 1 on success, exit otherwise
  */
-
-void display_error(const char *msg)
-{
-	fprintf(stderr, "%s\n", msg);
-	exit(ERR_INVALID_ELF);
-}
-
-/**
- * display_elf_header_info - Entry point of function
- * Description: used for executables, object code, shared libraries
- * @header: The header for data structure
- *
- * Return: Always 0.
- */
-
-void display_elf_header_info(Elf64_Ehdr *header)
-{
-	printf("Magic: %02x %02x %02x %02x\n",
-			header->e_ident[EI_MAG0],
-			header->e_ident[EI_MAG1],
-			header->e_ident[EI_MAG2],
-			header->e_ident[EI_MAG3]);
-
-	printf("Class: %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ?
-		       "ELF32" : "ELF64");
-	printf("Data: %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
-		       "2's complement, little-endian" : "2's complement, big-endian");
-	printf("Version: %d\n", header->e_ident[EI_VERSION]);
-
-	printf("OS/ABI: %d\n", header->e_ident[EI_OSABI]);
-
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-
-	printf("Type: %d\n", header->e_type);
-
-	printf("Entry point address: 0x%lx\n", header->e_entry);
-}
-
-/**
- * main -  The Entry point
- * @argc: Integer variable that holds the number of command-line arguments
- * @argv: Aarray of character pointers that hold the individual CL arguments
- *
- * Return: Always 0.
- */
-
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	int src, dest, n_read = 1024, wrote, close_src, close_dest;
+	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	char buffer[1024];
+
+	if (argc != 3)
 	{
-	display_error("Usage: elf_header elf_filename");
+		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-	const char *filename = argv[1];
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	src = open(argv[1], O_RDONLY);
+	check_IO_stat(src, -1, argv[1], 'O');
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	check_IO_stat(dest, -1, argv[2], 'W');
+	while (n_read == 1024)
 	{
-	display_error("Error opening the file");
+		n_read = read(src, buffer, sizeof(buffer));
+		if (n_read == -1)
+			check_IO_stat(-1, -1, argv[1], 'O');
+		wrote = write(dest, buffer, n_read);
+		if (wrote == -1)
+			check_IO_stat(-1, -1, argv[2], 'W');
 	}
-	Elf64_Ehdr header;
-	ssize_t num_read = read(fd, &header, sizeof(Elf64_Ehdr));
-
-	if (num_read != sizeof(Elf64_Ehdr))
-	{
-	display_error("Error reading ELF header");
-	}
-	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
-	{
-	display_error("Not an ELF file");
-	}
-
-	display_elf_header_info(&header);
-
-	close(fd);
-
+	close_src = close(src);
+	check_IO_stat(close_src, src, NULL, 'C');
+	close_dest = close(dest);
+	check_IO_stat(close_dest, dest, NULL, 'C');
 	return (0);
+}
+
+/**
+ * check_IO_stat - checks if a file can be opened or closed
+ * @stat: file descriptor of the file to be opened
+ * @filename: name of the file
+ * @mode: closing or opening
+ * @fd: file descriptor
+ *
+ * Return: void
+ */
+void check_IO_stat(int stat, int fd, char *filename, char mode)
+{
+	if (mode == 'C' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	else if (mode == 'O' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	else if (mode == 'W' && stat == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
 }
