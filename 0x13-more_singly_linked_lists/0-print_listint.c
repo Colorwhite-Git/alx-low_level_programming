66b2853d03959079c0c20a3bdf1e0df31f97fce2
Kwame <pauldasare@gmail.com>
2023-10-23 08:34:42 -0700

    Singly Corrections

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,10 +3,12 @@
 
 /**
  * print_listint - Print all elements of a linked list.
+ *
  * @h: Pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
@@ -17,5 +19,6 @@ size_t print_listint(const listint_t *h)
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
